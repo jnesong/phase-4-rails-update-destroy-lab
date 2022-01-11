5185dcf3988be87a94813c157a4a0cbf4e5f69bf
Jenny Chau <jnechau@gmail.com>
2022-01-10 22:49:42 -0500

    commit tree 090dbecb68e7af3252e6f2a3e2ea39c40136f1d0

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,7 +18,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: bird, status: :created
+    else
+      render json: {error: "plant not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "plant not found"}, status: :not_found
+    end
+  end
+
   private
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
